2282ad3906bfe39094bc2c0b5c579ab81d42d441
allison-0 <allisonulrich00@gmail.com>
2024-06-18 19:24:04 -0700

    msg

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 def main():
-    """
     ##################################################
     Comlete your code here
     Use m_perc, f_perc, nb_perc for your results
     ##################################################
-    """
-    """
+
+    m_perc = int(input('Enter the amount of male students:'))
+    f_perc = int(input'"Enter the amount of female students:)')
+    nb_perc = int(input('Enter the amount of non-binary students:'))
+
+
+
     ########################################
     # Do not delete the return statement
     ########################################
-    """
     print(
         f'The percentage of males, females and non-binary \t {m_perc: .2f} \t {f_perc: .2f} \t {nb_perc: .2f}')
     return m_perc, f_perc, nb_perc
