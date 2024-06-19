89e88d21f85291b7bf011f86ef48bbbad630ce9a
allison-0 <allisonulrich00@gmail.com>
2024-06-18 19:34:01 -0700

    msg

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     # Comlete your code here
     # Use m_perc, f_perc, nb_perc for your results
 
-    m_num = int(input('Enter the number of male students:'))
-    f_num = int(input('Enter the number of female students:'))
-    nb_num = int(input('Enter the number of non-binary students:'))
+    m_num = int(input('Enter the number of male students: '))
+    f_num = int(input('Enter the number of female students: '))
+    nb_num = int(input('Enter the number of non-binary students: '))
 
     # Calculate the percentages
 
@@ -14,11 +14,13 @@ def main():
     nb_perc = (nb_num / class_size) * 100
 
 
-    ########################################
+    ########################################20
     # Do not delete the return statement
     ########################################
+    print(f'The total number of students \t {class_size}')
+    print(f'The number of males, females, and non-binary \t {m_num} \t {f_num} \t {nb_num}')
     print(
-        f'The percentage of males, females and non-binary \t {m_perc: .2f} \t {f_perc: .2f} \t {nb_perc: .2f}')
+        f'The percentage of males, females and non-binary \t {m_perc: .2f}% \t {f_perc: .2f}% \t {nb_perc: .2f}%')
     return m_perc, f_perc, nb_perc
 
 
